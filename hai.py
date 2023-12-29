ca5cf42065d389ceef66191f529e1d0572eed5d1
SAI KIRAN <98216043+saikirantirukkovalluri@users.noreply.github.com>
2023-12-29 14:33:07 +0530

    Update hai.py

--- hai.py
+++ hai.py
@@ -2,3 +2,11 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title("Student management System")
 st.sidebar.title("File management system")
+#roll nuumber
+#name
+#fees
+st.sisdebar.subheader("Add Student")
+rollnumber=st.sidebar.number_input("Roll Number")
+name=st.sidebar.text_input("Nmae")
+fees=st.sidebar.number_input("Fees")
+add=st.sidebar.button("Add")
